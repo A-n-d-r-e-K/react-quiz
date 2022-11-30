026daaffd7ab4c4cfe20d7496092c74ede708c7a
Andrei K <275991@gmail.com>
2022-11-30 23:33:25 +0200

    format code

--- src/App.js
+++ src/App.js
@@ -1,51 +1,62 @@
-import React from 'react';
-import './index.scss';
+import React from "react";
+import "./index.scss";
 
 const questions = [
   {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
     correct: 0,
   },
   {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
     correct: 1,
   },
   {
-    title: 'Что такое JSX?',
+    title: "Что такое JSX?",
     variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
     ],
     correct: 2,
   },
 ];
 
-function Result({correct}) {
+function Result({ correct }) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
-<a href='/'>
-<button>Попробовать снова</button>
-</a>
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
     </div>
   );
 }
 
-function Game({step, question, onClickVariant}) {
-  const percentage = Math.round((step / questions.length)*100);
+function Game({ step, question, onClickVariant }) {
+  const percentage = Math.round((step / questions.length) * 100);
   return (
     <>
       <div className="progress">
-        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
       </div>
       <h1>{question.title}</h1>
       <ul>
         {question.variants.map((text, index) => (
-          <li onClick={() => onClickVariant (index) } key={text}>{text}</li>
+          <li onClick={() => onClickVariant(index)} key={text}>
+            {text}
+          </li>
         ))}
       </ul>
     </>
@@ -56,21 +67,20 @@ function App() {
   const [step, setStep] = React.useState(0);
   const [correct, setCorrect] = React.useState(0);
   const question = questions[step];
-const onClickVariant = (index) => {
-  setStep(step + 1);
+  const onClickVariant = (index) => {
+    setStep(step + 1);
 
-  if (index === question.correct) {
-    setCorrect(correct + 1);
-  }
-}
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  };
   return (
     <div className="App">
-      {
-        step !== questions.length ? (<Game step={step} question = {question} onClickVariant={onClickVariant}/>
-        ):(      
-        <Result correct={correct}/>
-        )
-      }
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} />
+      )}
     </div>
   );
 }
